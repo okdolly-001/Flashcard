a0387195287c0f26e5c135e3ad280cbb872a077a
Frush <cjfrush@ucdavis.edu>
2019-05-31 18:04:33 -0700

    Con Card Flips

--- src/Card.js
+++ src/Card.js
@@ -3,23 +3,24 @@ import './css/Card.css'
 import FlipSvg from './FlipSvg.js'
 import CorrectCard from './CorrectCard.js'
 class Card extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      flipped: false ,
       question: props.card.korean,
       answer: props.card.english,
       userInput: '',
       showAnswer: false,
       isCorrect: false
     }
+    this.flip = this.flip.bind(this);
   }
-
   showUserInput = e => {
     this.setState({ userInput: e.target.value })
   }
 
   flip = () => {
-    this.setState({ showAnswer: !this.state.showAnswer, isCorrect: false })
+    this.setState({ flipped: !this.state.flipped, showAnswer: !this.state.showAnswer, isCorrect: false })
   }
   validate = e => {
     if (this.state.userInput.length > 0 && e.key === 'Enter') {
@@ -32,26 +33,44 @@ class Card extends React.Component {
     }
   }
 
-  render () {
+  render() {
     const content = this.state.showAnswer
-      ? this.state.answer
-      : this.state.question
+    ? this.state.answer
+    : this.state.question
     console.log('content', content)
     return (
-      <div className='container'>
-        <div className='card'>
-          {this.state.isCorrect ? <CorrectCard /> : content}
-          <FlipSvg className='flip-svg' onClick={this.flip} />
-        </div>
-        <textarea
-          className='textarea-card'
-          onChange={this.showUserInput}
-          value={this.state.userInput}
-          onKeyDown={this.validate}
-        />
+      <div className="card">
+      <div onClick={this.flip} className={"card-container" + (this.state.flipped ? " flipped" : "")}>
+        {/* {this.state.isCorrect ? <CorrectCard /> : content} */}
+        <Front text={this.state.question}/>
+        <Back text={this.state.answer} />
+      </div>
+        <textarea className='textarea-card' onChange={this.showUserInput} value={this.state.userInput} onKeyDown={this.validate} />
       </div>
     )
   }
 }
 
+const Front = props => (
+      <div className="front">
+        {props.text} 
+      </div>
+)
+
+
+const Back = props => (
+  <div className="back">
+    {props.text}
+  </div>
+)
+
+// class Back extends React.Component {
+//   render() {
+//     return (
+//       <div className="back">
+//        {props.english}
+//       </div>
+//     )
+//   }
+// }
 export default Card
